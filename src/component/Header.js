ba21a91d1104377f72fab6c65a40413ffe49448d
flxk202122 <flxk202122@naver.com>
2023-12-21 20:37:16 +0900

    side tab 정리

--- src/component/Header.js
+++ src/component/Header.js
@@ -38,7 +38,7 @@ function Header(props) {
     // );
 
     return (
-        <header id="hd" className='active'>
+        <header id="hd" className='position-relative'>
             <div className='container d-flex align-items-center justify-content-between'>
                 <h1><a href=""><span className='visually-hidden'>슬로우캘리 로고이미지</span></a></h1>
                 <ul id="gnb" className='d-flex align-item-center justify-content-between'>
@@ -63,33 +63,35 @@ function Header(props) {
                         })
                     }
                 </ul>
-                <div id="gnb_tab">
+                <div id="gnb_tab" className='show'>
                     <button className='tab_btn'>
                         <span className='visually-hidden'>메뉴창 열기</span>
                     </button>
-                    <div className='tab_box position-absolute d-none'>
-                        <ul id="tab_list">
-                            {
-                                props.datasrc.gnb.map((el, idx) => {
-                                    return (
-                                        <li key={idx} className={clsx('position-relative', el.gnb_cls)}>
-                                            <a href={el.gnb_href}>{el.gnb_nm}</a>
-                                            <ul className='ul2d position-absolute'>
-                                                {
-                                                    el.ultwo.map((eel, iidx) => {
-                                                        return (
-                                                            <li>
-                                                                <a href={eel.gnb_href}>{eel.gnb_nm}</a>
-                                                            </li>
-                                                        )
-                                                    })
-                                                }
-                                            </ul>
-                                        </li>
-                                    )
-                                })
-                            }
-                        </ul>
+                    <div className='tab_box position-absolute'>
+                        <div className='container'>
+                            <ul id="tab_list">
+                                {
+                                    props.datasrc.gnb.map((el, idx) => {
+                                        return (
+                                            <li key={idx} className={clsx('position-relative', el.gnb_cls)}>
+                                                <a href={el.gnb_href}>{el.gnb_nm}</a>
+                                                <ul className='ul2d position-absolute d-flex'>
+                                                    {
+                                                        el.ultwo.map((eel, iidx) => {
+                                                            return (
+                                                                <li>
+                                                                    <a href={eel.gnb_href}>{eel.gnb_nm}</a>
+                                                                </li>
+                                                            )
+                                                        })
+                                                    }
+                                                </ul>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
