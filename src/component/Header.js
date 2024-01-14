ab0c6ebee2710c04f499bc35e077c38a7c0a2359
flxk202122 <flxk202122@naver.com>
2024-01-14 11:44:30 +0900

    반응형 작업, usestate 활용한 탭작업중

--- src/component/Header.js
+++ src/component/Header.js
@@ -7,97 +7,97 @@ import { Link } from 'react-router-dom';
 
 function Header(props) {
 
-    const [handOn, sethandOn] = useState(false);
-    const [toggleBtn, settoggleBtn] = useState(false);
-    const [showTab, setshowTab] = useState(0);
+	const [handOn, sethandOn] = useState(false);
+	const [toggleBtn, settoggleBtn] = useState(false);
+	const [showTab, setshowTab] = useState(0);
 
 
 
-    const handleMouseOver = () => {
-        sethandOn(true);
-    };
+	const handleMouseOver = () => {
+		sethandOn(true);
+	};
 
-    const handleMouseOut = () => {
-        sethandOn(false);
-    };
-
-
+	const handleMouseOut = () => {
+		sethandOn(false);
+	};
 
 
-    return (
-        <header id="hd"
-            onMouseOver={handleMouseOver}
-            onMouseOut={handleMouseOut}
-            className={`position-relative ${!toggleBtn && handOn ? "active" : ""} ${toggleBtn ? "tabopen" : ""}`}>
-            <div className='container d-flex align-items-center justify-content-between'>
-                <h1><Link to="/"><span className='visually-hidden'>슬로우캘리 로고이미지</span></Link></h1>
-                <ul id="gnb" className='d-flex align-item-center justify-content-between'>
-                    {
-                        props.datasrc.gnb.map((el, idx) => {
-                            return (
-                                <li key={idx} className={clsx('position-relative d-none d-lg-block', el.gnb_cls)}>
-                                    <Link to={`/${el.gnb_href}`}>{el.gnb_nm}</Link>
-                                    <ul className='ul2d position-absolute'>
-                                        {
-                                            el.ultwo.map((eel, iidx) => {
-                                                return (
-                                                    <li key={iidx}>
-                                                        <Link to={`/${eel.gnb_href}`}>{eel.gnb_nm}</Link>
-                                                    </li>
-                                                )
-                                            })
-                                        }
-                                    </ul>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-                <div id="gnb_tab">
-                    <button className='tab_btn' onClick={() => settoggleBtn(!toggleBtn)}>
-                        <span className='visually-hidden'>메뉴창 열기</span>
-                    </button>
-                    <div className='tab_box position-absolute'>
-                        <div className='container position-relative'>
-                            <ul id="tab_list">
-                                {
-                                    props.datasrc.gnb.map((el, idx) => {
-                                        return (
-                                            <li key={idx} className={`${el.gnb_cls} ${showTab === idx ? "active" : ""}`} onClick={() => { setshowTab(idx) }}>
-                                                <Link to="/">{el.gnb_nm}</Link>
-                                                {showTab === idx &&
-                                                    <ul className="ul2d position-absolute">
-                                                        {
-                                                            el.ultwo.map((eel, iidx) => {
-                                                                return (
-                                                                    <li key={iidx}>
-                                                                        <Link to="/">{eel.gnb_nm}</Link>
-                                                                    </li>
-                                                                )
-                                                            })
-                                                        }
-                                                    </ul>
-                                                }
-                                                {showTab === idx &&
-                                                    <div className='bg_box position-absolute' style={{
-                                                        backgroundImage: `url(./img/${el.gnb_bg})`,
-                                                        backgroundRepeat: 'no-repeat',
-                                                        backgroundSize: 'cover',
-                                                        backgroundPosition: 'center'
-                                                    }}>
-                                                    </div>
-                                                }
-                                            </li>
-                                        )
-                                    })
-                                }
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-    )
+
+
+	return (
+		<header id="hd"
+			onMouseOver={handleMouseOver}
+			onMouseOut={handleMouseOut}
+			className={`position-relative ${!toggleBtn && handOn ? "active" : ""} ${toggleBtn ? "tabopen" : ""}`}>
+			<div className='container d-flex align-items-center justify-content-between'>
+				<h1><Link to="/"><span className='visually-hidden'>슬로우캘리 로고이미지</span></Link></h1>
+				<ul id="gnb" className='d-flex align-item-center justify-content-between'>
+					{
+						props.datasrc.gnb.map((el, idx) => {
+							return (
+								<li key={idx} className={clsx('position-relative d-none d-lg-block', el.gnb_cls)}>
+									<Link to={`/${el.gnb_href}`}>{el.gnb_nm}</Link>
+									<ul className='ul2d position-absolute'>
+										{
+											el.ultwo.map((eel, iidx) => {
+												return (
+													<li key={iidx}>
+														<Link to={`/${eel.gnb_href}`}>{eel.gnb_nm}</Link>
+													</li>
+												)
+											})
+										}
+									</ul>
+								</li>
+							)
+						})
+					}
+				</ul>
+				<div id="gnb_tab">
+					<button className='tab_btn' onClick={() => settoggleBtn(!toggleBtn)}>
+						<span className='visually-hidden'>메뉴창 열기</span>
+					</button>
+					<div className='tab_box position-absolute'>
+						<div className='container position-relative'>
+							<ul id="tab_list">
+								{
+									props.datasrc.gnb.map((el, idx) => {
+										return (
+											<li key={idx} className={`${el.gnb_cls} ${showTab === idx ? "active" : ""}`} onClick={() => { setshowTab(idx) }}>
+												<Link to="/">{el.gnb_nm}</Link>
+												{showTab === idx &&
+													<ul className="ul2d position-absolute">
+														{
+															el.ultwo.map((eel, iidx) => {
+																return (
+																	<li key={iidx}>
+																		<Link to="/">{eel.gnb_nm}</Link>
+																	</li>
+																)
+															})
+														}
+													</ul>
+												}
+												{showTab === idx &&
+													<div className='bg_box position-absolute' style={{
+														backgroundImage: `url(./img/${el.gnb_bg})`,
+														backgroundRepeat: 'no-repeat',
+														backgroundSize: 'cover',
+														backgroundPosition: 'center'
+													}}>
+													</div>
+												}
+											</li>
+										)
+									})
+								}
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	)
 }
 
 export default Header
--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,108 +1,125 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom'
 
 import './scss/menu.scss';
 
 function Menu(props) {
-    return (
-        <section id='menu'>
-            <div className='container position-relative'>
-                <h2 className='visually-hidden'>{props.datasrc.h2}</h2>
-                <ul className='subject_list d-flex justify-content-center'>
-                    {
-                        props.datasrc.subject_list.map((el, idx) => {
-                            return (
-                                <li className='position-relative' key={idx}>{el.nm}</li >
-                            )
-                        })
-                    }
-                </ul >
-            </div >
-            <div className='bg_box position-relative'>
-                {
-                    props.datasrc.subject_list.map((el, idx) => {
-                        return (
-                            <img key={idx} src={`./img/menu/${el.bg}`} alt="" />
-                        )
-                    })
-                }
-            </div>
-            <div className='container position-relative'>
-                <div className='top_box d-none'>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </div>
-                <ul className='kal_list d-flex position-relative'>
-                    {
-                        props.datasrc.menu_list.map((el, idx) => {
-                            return (
-                                <li key={idx} className='col-3'><Link to={el.href}>{el.nm}</Link>
-                                    <ul className='box'>
-                                        {
-                                            el.menu_content.map((eel, iidx) => {
-                                                return (
-                                                    <li key={iidx}><Link>{eel.menu_nm}</Link>
-                                                        <div className='d-none'>
-                                                            <img src={`./img/menu/${eel.menu_bg}`} alt="" />
-                                                        </div>
-                                                        <ul className='kal_box'>
-                                                            <li className='d-flex justify-content-between'>
-                                                                <span>총g</span>
-                                                                <span>{eel.menu_all}</span>
-                                                            </li>
-                                                            <li className='d-flex justify-content-between'>
-                                                                <span>열량&#40;Kcal&#41;</span>
-                                                                <span>{eel.menu_kcal}</span>
-                                                            </li>
-                                                            <li className='d-flex justify-content-between'>
-                                                                <span>탄수화물&#40;g&#41;</span>
-                                                                <span>{eel.menu_tan}</span>
-                                                            </li>
-                                                            <li className='d-flex justify-content-between'>
-                                                                <span>단백질&#40;g&#41;</span>
-                                                                <span>{eel.menu_dan}</span>
-                                                            </li>
-                                                            <li className='d-flex justify-content-between'>
-                                                                <span>지방&#40;g&#41;</span>
-                                                                <span>{eel.menu_zi}</span>
-                                                            </li>
-                                                            <li className='d-flex justify-content-between'>
-                                                                <span>나트륨&#40;g&#41;</span>
-                                                                <span>{eel.menu_na}</span>
-                                                            </li>
-                                                            <li className='d-flex justify-content-between'>
-                                                                <span>당&#40;g&#41;</span>
-                                                                <span>{eel.menu_dang}</span>
-                                                            </li>
-                                                        </ul>
-                                                    </li>
-                                                )
-                                            })
-                                        }
-                                    </ul>
-                                </li>
-                            )
-                        })
 
-                    }
-                </ul>
-            </div >
-        </section >
-    )
+	const [menuTab, setmenuTab] = useState(0);
+	const [subTab, setsubTab] = useState(0);
+
+
+
+
+	return (
+		<section id='menu'>
+			<div className='container position-relative'>
+				<h2 className='visually-hidden'>{props.datasrc.h2}</h2>
+				<ul className='subject_list d-flex justify-content-center'>
+					{
+						props.datasrc.subject_list.map((el, idx) => {
+							return (
+								<li className='position-relative' key={idx} onClick={() => { setmenuTab(idx) }}><Link to=''>{el.nm}</Link></li >
+							)
+						})
+					}
+				</ul >
+			</div >
+			<div className='bg_box'>
+				{
+					props.datasrc.subject_list.map((el, idx) => {
+						return (
+							<img key={idx} src={`./img/menu/${el.bg}`} className={`${menuTab === idx ? "active" : ""}`} alt="" />
+						)
+					})
+
+				}
+			</div>
+			<div className='container position-relative'>
+				<div className='top_box'>
+					<h3>Topping</h3>
+					<ul className='d-flex'>
+						{
+							props.datasrc.topping_list.map((el, idx) => {
+								return (
+									<li key={idx}><Link to=''>{el.nm}</Link>
+										<img src={`./img/menu/${el.bg}`} alt="" />
+									</li>
+								)
+							})
+						}
+						<li></li>
+					</ul>
+				</div>
+				<div>
+					<h3 className=''>MENU</h3>
+					<ul className='tab_list d-flex justify-content-center'>
+						{
+							props.datasrc.menu_list.map((el, idx) => {
+								return (
+									<li onClick={() => { setsubTab(idx) }}><Link to={el.href}>{el.nm}</Link></li>
+								)
+							})
+						}
+					</ul>
+					<ul className='kal_list'>
+						{
+							props.datasrc.menu_list.map((el, idx) => {
+								return (
+									<li key={idx} className={`${subTab === idx ? "active" : ""}`} >
+										<ul className='box d-md-flex justify-content-center'>
+											{
+												el.menu_content.map((eel, iidx) => {
+													return (
+														<li key={iidx} className='position-relative row col-md-4 '>
+															<Link className='col-12 d-flex justify-content-center align-items-center'>{eel.menu_nm}</Link>
+															<div className='menu_box col-6 col-md-12 align-items-center'>
+																<img className='w-100' src={`./img/menu/${eel.menu_bg}`} alt="" />
+															</div>
+															<ul className='kal_box col-6 col-md-12'>
+																<li className='d-flex justify-content-between'>
+																	<span>총g</span>
+																	<span>{eel.menu_all}</span>
+																</li>
+																<li className='d-flex justify-content-between'>
+																	<span>열량&#40;Kcal&#41;</span>
+																	<span>{eel.menu_kcal}</span>
+																</li>
+																<li className='d-flex justify-content-between'>
+																	<span>탄수화물&#40;g&#41;</span>
+																	<span>{eel.menu_tan}</span>
+																</li>
+																<li className='d-flex justify-content-between'>
+																	<span>단백질&#40;g&#41;</span>
+																	<span>{eel.menu_dan}</span>
+																</li>
+																<li className='d-flex justify-content-between'>
+																	<span>지방&#40;g&#41;</span>
+																	<span>{eel.menu_zi}</span>
+																</li>
+																<li className='d-flex justify-content-between'>
+																	<span>나트륨&#40;g&#41;</span>
+																	<span>{eel.menu_na}</span>
+																</li>
+																<li className='d-flex justify-content-between'>
+																	<span>당&#40;g&#41;</span>
+																	<span>{eel.menu_dang}</span>
+																</li>
+															</ul>
+														</li>
+													)
+												})
+											}
+										</ul>
+									</li>
+								)
+							})
+						}
+					</ul>
+				</div>
+			</div >
+		</section >
+	)
 }
 
 export default Menu
