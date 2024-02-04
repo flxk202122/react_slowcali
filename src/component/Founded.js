e79bc6f92282eb56ab639d0b51dc935de9ff3775
flxk202122 <flxk202122@naver.com>
2024-02-04 13:36:25 +0900

    깃 중간 빌드

--- src/component/Founded.js
+++ src/component/Founded.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 import './scss/founded.scss'
 
 function Founded(props) {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
     return (
         <section className='founded position-relative overflow-hidden'>
             <div className="container position-relative">
@@ -11,14 +18,14 @@ function Founded(props) {
                     <p className='w-100 text-center'>{props.datasrc.founded_txt}</p>
                 </div>
                 <div className='founded_step'>
-                    <ul className='row mx-0 justify-content-between'>
+                    <ul className='d-flex row mx-0 justify-content-lg-between justify-content-around'>
                         {
                             props.datasrc.founded_list.map((el, idx) => {
                                 return (
-                                    <li key="idx" className='position-relative d-flex flex-column  align-items-center justify-content-center col-2 mx-3 mb-5'>
+                                    <li key="idx" className='position-relative d-flex flex-lg-column  align-items-center justify-content-lg-center justify-content-around col-lg-2 col-12 mx-2 mb-lg-5 mb-2'>
                                         <span className='num_list position-absolute text-center'>{idx + 1}</span>
-                                        <img src={`./img/${el.founded_bg}`} alt="" />
-                                        <p className='d-flex row text-center'>
+                                        <img className='col-lg-auto col-4' src={`./img/${el.founded_bg}`} alt="" />
+                                        <p className='row text-lg-center col-lg-auto col-8'>
                                             <span className='list_nm'>{el.founded_nm}</span>
                                             <span>{el.founded_txt}</span>
                                         </p>
@@ -36,6 +43,55 @@ function Founded(props) {
                         <span>슬로우캘리와 함께하세요.</span>
                     </div>
                     <div className='together_form'>
+                        <form onSubmit={handleSubmit(onSubmit)} className=''>
+                            <fieldset className='position-absolute'>창업문의</fieldset>
+                            <ul className='position-relative'>
+                                <li className='d-flex position-relative'>
+                                    <strong className='col-4'>고객명</strong>
+                                    <input className='col-8' type="text" {...register('wr_name')} />
+                                </li>
+                                <li className='d-flex position-relative'>
+                                    <strong className='col-4'>연락처</strong>
+                                    <input className='col-8' type="text" {...register('wr_number')} />
+                                </li>
+                                <li className='d-flex position-relative'>
+                                    <strong className='col-4'>이메일</strong>
+                                    <input className='col-8' type="text" {...register('wr_email')} />
+                                </li>
+                                <li className='d-flex position-relative'>
+                                    <strong className='col-4'>연락 가능 시간</strong>
+                                    <input className='col-8' type="text" {...register('wr_time')} />
+                                </li>
+                                <li className='d-flex position-relative'>
+                                    <strong className='col-4'>창업희망지역</strong>
+                                    <input className='col-8' type="text" {...register('wr_location')} />
+                                </li>
+                                <li className='d-flex position-relative'>
+                                    <strong className='col-4'>점포보유유무</strong>
+                                    <div className='col-4'>
+                                        <label htmlFor="no_store" className='px-2'>없음</label>
+                                        <input type="radio" id='no_store' {...register('store_yn', { value: 'no' })} />
+                                    </div>
+                                    <div className='col-4'>
+                                        <label htmlFor="yes_store" className='px-2'>있음</label>
+                                        <input type="radio" id='yes_store' {...register('store_yn', { value: 'yes' })} />
+                                    </div>
+                                </li>
+                                <li className='d-flex position-relative'>
+                                    <strong className='col-4'>상세문의 내용</strong>
+                                    <textarea className='col-8' {...register('wr_details')} />
+                                </li>
+                            </ul>
+                            <div className='btns position-relative d-flex align-items-center justify-content-between'>
+                                <div className='btn_a col-5 justify-content-around d-flex'>
+                                    <label htmlFor="ok_myadmin">개인정보 수집 및 이용동의</label>
+                                    <input type="radio" id='ok_myadmin' {...register('ok_myadmin', { required: true })} />
+                                </div>
+                                <button type="submit" className='btn_b col-5'>제출하기</button>
+                            </div>
+                        </form>
+                    </div>
+                    {/* <div className='together_form'>
                         <form action="" className=''>
                             <fieldset className='position-absolute'>창업문의</fieldset>
                             <ul className='position-relative'>
@@ -86,7 +142,7 @@ function Founded(props) {
                             </button>
                         </div>
 
-                    </div>
+                    </div> */}
                 </div>
 
             </div>
